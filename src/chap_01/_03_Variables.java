fd203408741f2ddba2c438c40c18ad0d93600f18
taewoo <rlaxodn322@gmail.com>
2024-10-04 11:49:13 +0900

    Variables

---
+++ src/chap_01/_03_Variables.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+         String name = "태우";
+        int hour  = 12;
+        System.out.println(name + "님 배송이 "+ hour+"시에 시작됩니다.");
+        double score = 90.2;
+        char grade = 'A';
+
+        System.out.println("나의 점수는 " + score);
+        System.out.println("나으 학점은 " + grade);
+    }
+}
