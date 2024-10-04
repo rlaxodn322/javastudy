05eabf4b722be1939576effdda724bbf8442b547
taewoo <rlaxodn322@gmail.com>
2024-10-04 11:43:17 +0900

    study 2

---
+++ src/chap_01/_02_Datatype.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _02_Datatype {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println(12);
+        System.out.println("12");
+        System.out.println(true);
+        System.out.println("true");
+        System.out.println(123 +123);
+
+    }
+}
